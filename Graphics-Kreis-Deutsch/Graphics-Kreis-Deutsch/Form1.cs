a3b4aeff998d70576d3ccd23517266b92883c737
Nicole Deutsch <54978835+Elocin727@users.noreply.github.com>
2019-09-27 09:44:57 +0200

    Kreis in der Mitte ausgefüllt mit Rand

---
+++ Graphics-Kreis-Deutsch/Graphics-Kreis-Deutsch/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics_Kreis_Deutsch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graphics = e.Graphics;
+            Pen circlePen = new Pen(Color.Black,5f);
+            Rectangle rect = new Rectangle((this.ClientSize.Width/2-100), (this.ClientSize.Height/2-100), 200, 200);
+            SolidBrush ellipseBrush = new SolidBrush(Color.Red);
+            graphics.FillEllipse(ellipseBrush, rect);
+            e.Graphics.DrawEllipse(circlePen, rect);
+        }
+
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+    }
+}
